ddc5ebad0a236272471a7873801ab4b59c6e7dec
Don Denton <don.denton@trillianthealth.com>
2023-01-08 00:08:17 -0500

    RSS

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,21 @@
+import type {MarkdownInstance} from 'astro';
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+
+// Works with Markdown files only!
+const postImportResult = import.meta.glob<true, string, MarkdownInstance<{title: string, date: string}>>("./20[0-9][0-9]/**/*.md", { eager: true }); 
+const posts = Object.values(postImportResult);
+
+console.log(posts.map(x=>x.frontmatter.date))
+
+export const get = () => rss({
+  title: 'Happy Collision',
+  description: 'The mostly technical musings of Don Denton.',
+  site: import.meta.env.SITE,
+  items: posts.map((post) => ({
+    link: post.url as string,
+    title: post.frontmatter.title,
+    pubDate: new Date(post.frontmatter.date),
+    content: sanitizeHtml(post.compiledContent()),
+  }))
+});

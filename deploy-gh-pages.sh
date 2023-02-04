e7b30d9707c9c7f1c3a7cb15eeab93ad57983d31
Don Denton <don@happycollision.com>
2023-02-03 23:55:01 -0500

    improve deploy: remove dropped files

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -14,12 +14,15 @@ rm -rf $directory
 echo -e "\033[0;32mChecking out $branch....\033[0m"
 git worktree add $directory $branch
 
+echo -e "\033[0;32mCleaning $directory...\033[0m"
+cd $directory &&
+  git rm -rf *
+
 echo -e "\033[0;32mGenerating site...\033[0m"
 build_command
 
 echo -e "\033[0;32mDeploying $branch branch...\033[0m"
-cd $directory &&
-  git add --all &&
+git add --all &&
   git commit -m "Deploy updates" &&
   git push origin $branch
 

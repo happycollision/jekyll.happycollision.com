628c476e44cc134e2513c1a698212a9533d26213
Don Denton <don@happycollision.com>
2023-02-13 10:21:21 -0500

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://happycollision.com",
-  integrations: [mdx()],
+  integrations: [mdx(), tailwind()],
 })

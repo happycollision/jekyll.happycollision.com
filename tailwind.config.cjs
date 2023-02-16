4ab84744361b2220cf35684f50886e14f2d0edc5
Don Denton <don@happycollision.com>
2023-02-16 09:57:04 -0500

    move .link to utility

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addUtilities, theme }) {
+      addUtilities({
+        ".link": {
+          color: theme("colors.blue.500"),
+          textDecoration: "underline",
+        },
+      })
+    },
+  ],
 }
